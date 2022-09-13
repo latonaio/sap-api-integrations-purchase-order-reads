1f6e5a60ec2d5d4be9dd5753fa33ff1353ee68a1
Yurino Oga <yurino.o@latona.io>
2022-09-13 13:07:21 +0900

    release

---
+++ README.md
@@ -0,0 +1,143 @@
+# sap-api-integrations-purchase-order-reads
+sap-api-integrations-purchase-order-reads は、他のすべての sap-api-integrations-purchase-order-reads 作成更新の際の 参照元となる マスタレポジトリです。  
+sap-api-integrations-purchase-order-reads は、外部システム(特にエッジコンピューティング環境)をSAPと統合することを目的に、SAP API で 購買発注データを取得するマイクロサービスです。    
+sap-api-integrations-purchase-order-reads には、サンプルのAPI Json フォーマットが含まれています。   
+sap-api-integrations-purchase-order-reads は、オンプレミス版である（＝クラウド版ではない）SAPS4HANA API の利用を前提としています。クラウド版APIを利用する場合は、ご注意ください。   
+https://api.sap.com/api/OP_API_PURCHASEORDER_PROCESS_SRV_0001/overview  
+
+## 動作環境  
+sap-api-integrations-purchase-order-reads は、主にエッジコンピューティング環境における動作にフォーカスしています。  
+使用する際は、事前に下記の通り エッジコンピューティングの動作環境（推奨/必須）を用意してください。  
+・ エッジ Kubernetes （推奨）    
+・ AION のリソース （推奨)    
+・ OS: LinuxOS （必須）    
+・ CPU: ARM/AMD/Intel（いずれか必須）　　
+
+## クラウド環境での利用
+sap-api-integrations-purchase-order-reads は、外部システムがクラウド環境である場合にSAPと統合するときにおいても、利用可能なように設計されています。  
+
+## 本レポジトリ が 対応する API サービス
+sap-api-integrations-purchase-order-reads が対応する APIサービス は、次のものです。
+
+* APIサービス概要説明 URL: https://api.sap.com/api/OP_API_PURCHASEORDER_PROCESS_SRV_0001/overview    
+* APIサービス名(=baseURL): API_PURCHASEORDER_PROCESS_SRV
+
+## 本レポジトリ に 含まれる API名
+sap-api-integrations-purchase-order-reads には、次の API をコールするためのリソースが含まれています。  
+
+* A_PurchaseOrder（購買発注 - ヘッダ）※購買発注関連データを取得するために、ToItem、ToItemScheduleLine、ToItemPricingElement、ToItemPricingAccountと合わせて利用されます。  
+* ToItem（購買発注 - 明細）
+* ToItemScheduleLine（購買発注 - 納入日程行）
+* ToItemPricingElement（購買発注 - 価格条件）
+* ToItemAccount（購買発注 - 勘定設定）
+* A_PurchaseOrderItem（購買発注 - 明細）※購買発注関連データを取得するために、ToItemScheduleLine、ToItemPricingElement、ToItemPricingElementと合わせて利用されます。  
+* ToItemScheduleLine（購買発注 - 納入日程行）
+* ToItemPricingElement（購買発注 - 価格条件）
+* ToItemAccount（購買発注 - 勘定設定）
+* A_PurchaseOrderScheduleLine（購買発注 - 納入日程行）
+* A_PurOrdPricingElement（購買発注 - 価格条件）
+* A_PurOrdAccountAssignment（購買発注 - 勘定設定）
+
+## API への 値入力条件 の 初期値
+sap-api-integrations-purchase-order-reads において、API への値入力条件の初期値は、入力ファイルレイアウトの種別毎に、次の通りとなっています。  
+
+### SDC レイアウト
+
+* inputSDC.PurchaseOrder.PurchaseOrder（購買発注）,
+* inputSDC.PurchaseOrder. PurchaseOrderItePurchaseOrderItem（購買発注明細）,
+* inputSDC.PurchaseOrder.PurchaseOrderIteItemScheduleLine.PurchasingDocument（購買伝票 ※購買発注の納入日程行のAPIをコールするときに購買発注ではなく購買伝票としての項目値が必要です。通常は、購買伝票の値＝購買発注の値、となります）,
+* inputSDC.PurchaseOrder. PurchaseOrderIteItemScheduleLine. PurchasingDocumentItem（購買伝票明細 ※購買発注の納入日程行のAPIをコールするときに購買発注明細ではなく購買伝票明細としての項目値が必要です。通常は、購買伝票明細の値＝購買発注明細の値、となります）,
+* inputSDC.PurchaseOrder. PurchaseOrderItePurchaseRequisition（購買依頼）,
+* inputSDC.PurchaseOrder. PurchaseOrderItePurchaseRequisitionItem（購買依頼明細）,
+
+## SAP API Bussiness Hub の API の選択的コール
+
+Latona および AION の SAP 関連リソースでは、Inputs フォルダ下の sample.json の accepter に取得したいデータの種別（＝APIの種別）を入力し、指定することができます。  
+なお、同 accepter にAll(もしくは空白)の値を入力することで、全データ（＝全APIの種別）をまとめて取得することができます。  
+
+* sample.jsonの記載例(1)  
+
+accepter において 下記の例のように、データの種別（＝APIの種別）を指定します。  
+ここでは、"Header" が指定されています。
+
+```
+	"api_schema": "sap.s4.beh.purchaseorder.v1.PurchaseOrder.Created.v1",
+	"accepter": ["Header"],
+	"purchase_order": "4500000028",
+	"deleted": false
+```
+  
+* 全データを取得する際のsample.jsonの記載例(2)  
+
+全データを取得する場合、sample.json は以下のように記載します。  
+
+```
+	"api_schema": "sap.s4.beh.purchaseorder.v1.PurchaseOrder.Created.v1",
+	"accepter": ["All"],
+	"purchase_order": "4500000028",
+	"deleted": false
+```
+
+## 指定されたデータ種別のコール
+
+accepter における データ種別 の指定に基づいて SAP_API_Caller 内の caller.go で API がコールされます。  
+caller.go の func() 毎 の 以下の箇所が、指定された API をコールするソースコードです。  
+
+```
+func (c *SAPAPICaller) AsyncGetPurchaseOrder(purchaseOrder, purchaseOrderItem, purchasingDocument, purchasingDocumentItem, purchaseRequisition, purchaseRequisitionItem string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(purchaseOrder)
+				wg.Done()
+			}()
+		case "Item":
+			func() {
+				c.Item(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "ItemScheduleLine":
+			func() {
+				c.ItemScheduleLine(purchasingDocument, purchasingDocumentItem)
+				wg.Done()
+			}()
+		case "ItemPricingElement":
+			func() {
+				c.ItemPricingElement(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "ItemAccount":
+			func() {
+				c.ItemAccount(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "PurchaseRequisition":
+			func() {
+				c.PurchaseRequisition(purchaseRequisition, purchaseRequisitionItem)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+```
+## Output  
+本マイクロサービスでは、[golang-logging-library](https://github.com/latonaio/golang-logging-library) により、以下のようなデータがJSON形式で出力されます。  
+以下の sample.json の例は、SAP 購買発注 の ヘッダデータ が取得された結果の JSON の例です。  
+以下の項目のうち、"PurchaseOrder" ～ "to_PurchaseOrderItem" は、/SAP_API_Output_Formatter/type.go 内 の Type Header {} による出力結果です。"cursor" ～ "time"は、golang-logging-library による 定型フォーマットの出力結果です。  
+
+```
+{
+	"cursor": "/Users/latona2/bitbucket/sap-api-integrations-purchase-order-reads/SAP_API_Caller/caller.go#L50",
+	"function": "sap-api-integrations-purchase-order-reads/SAP_API_Caller.(*SAPAPICaller).Header",
+	"level": "INFO",
+	"message": "[{XXXXXXXXXXXXXXXXXXXXXXXXXXXXX}]",
+	"time": "2021-12-11T15:33:00.054455+09:00"
+}
+```
---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,396 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	sap_api_output_formatter "sap-api-integrations-purchase-order-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	sap_api_request_client_header_setup "github.com/latonaio/sap-api-request-client-header-setup"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+)
+
+type SAPAPICaller struct {
+	baseURL         string
+	sapClientNumber string
+	requestClient   *sap_api_request_client_header_setup.SAPRequestClient
+	log             *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl, sapClientNumber string, requestClient *sap_api_request_client_header_setup.SAPRequestClient, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL:         baseUrl,
+		requestClient:   requestClient,
+		sapClientNumber: sapClientNumber,
+		log:             l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetPurchaseOrder(purchaseOrder, purchaseOrderItem, purchasingDocument, purchasingDocumentItem, purchaseRequisition, purchaseRequisitionItem string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "Header":
+			func() {
+				c.Header(purchaseOrder)
+				wg.Done()
+			}()
+		case "Item":
+			func() {
+				c.Item(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "ItemScheduleLine":
+			func() {
+				c.ItemScheduleLine(purchasingDocument, purchasingDocumentItem)
+				wg.Done()
+			}()
+		case "ItemPricingElement":
+			func() {
+				c.ItemPricingElement(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "ItemAccount":
+			func() {
+				c.ItemAccount(purchaseOrder, purchaseOrderItem)
+				wg.Done()
+			}()
+		case "PurchaseRequisition":
+			func() {
+				c.PurchaseRequisition(purchaseRequisition, purchaseRequisitionItem)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) Header(purchaseOrder string) {
+	headerData, err := c.callPurchaseOrderSrvAPIRequirementHeader("A_PurchaseOrder", purchaseOrder)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(headerData)
+
+	itemData, err := c.callToItem(headerData[0].ToItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+
+	itemScheduleLineData, err := c.callToItemScheduleLine(itemData[0].ToItemScheduleLine)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemScheduleLineData)
+
+	itemPricingElementData, err := c.callToItemPricingElement(itemData[0].ToItemPricingElement)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemPricingElementData)
+
+	itemAccountData, err := c.callToItemAccount(itemData[0].ToItemAccount)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemAccountData)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementHeader(api, purchaseOrder string) ([]sap_api_output_formatter.Header, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+	param := c.getQueryWithHeader(map[string]string{}, purchaseOrder)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToHeader(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItem(url string) ([]sap_api_output_formatter.ToItem, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemScheduleLine(url string) ([]sap_api_output_formatter.ToItemScheduleLine, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItemScheduleLine(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemPricingElement(url string) ([]sap_api_output_formatter.ToItemPricingElement, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItemPricingElement(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItemAccount(url string) ([]sap_api_output_formatter.ToItemAccount, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItemAccount(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) Item(purchaseOrder, purchaseOrderItem string) {
+	itemData, err := c.callPurchaseOrderSrvAPIRequirementItem("A_PurchaseOrderItem", purchaseOrder, purchaseOrderItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+
+	itemScheduleLineData, err := c.callToItemScheduleLine(itemData[0].ToItemScheduleLine)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemScheduleLineData)
+
+	itemPricingElementData, err := c.callToItemPricingElement(itemData[0].ToItemPricingElement)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemPricingElementData)
+
+	itemAccountData, err := c.callToItemAccount(itemData[0].ToItemAccount)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemAccountData)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementItem(api, purchaseOrder, purchaseOrderItem string) ([]sap_api_output_formatter.Item, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+
+	param := c.getQueryWithItem(map[string]string{}, purchaseOrder, purchaseOrderItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) ItemScheduleLine(purchasingDocument, purchasingDocumentItem string) {
+	data, err := c.callPurchaseOrderSrvAPIRequirementItemScheduleLine("A_PurchaseOrderScheduleLine", purchasingDocument, purchasingDocumentItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementItemScheduleLine(api, purchasingDocument, purchasingDocumentItem string) ([]sap_api_output_formatter.ItemScheduleLine, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+
+	param := c.getQueryWithItemScheduleLine(map[string]string{}, purchasingDocument, purchasingDocumentItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItemScheduleLine(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) ItemPricingElement(purchaseOrder, purchaseOrderItem string) {
+	data, err := c.callPurchaseOrderSrvAPIRequirementItemPricingElement("A_PurOrdPricingElement", purchaseOrder, purchaseOrderItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementItemPricingElement(api, purchaseOrder, purchaseOrderItem string) ([]sap_api_output_formatter.ItemPricingElement, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+
+	param := c.getQueryWithItemPricingElement(map[string]string{}, purchaseOrder, purchaseOrderItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItemPricingElement(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) ItemAccount(purchaseOrder, purchaseOrderItem string) {
+	data, err := c.callPurchaseOrderSrvAPIRequirementItemAccount("A_PurOrdAccountAssignment", purchaseOrder, purchaseOrderItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementItemAccount(api, purchaseOrder, purchaseOrderItem string) ([]sap_api_output_formatter.ItemAccount, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+
+	param := c.getQueryWithItemAccount(map[string]string{}, purchaseOrder, purchaseOrderItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItemAccount(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) PurchaseRequisition(purchaseRequisition, purchaseRequisitionItem string) {
+	data, err := c.callPurchaseOrderSrvAPIRequirementPurchaseRequisition("A_PurchaseOrderItem", purchaseRequisition, purchaseRequisitionItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(data)
+}
+
+func (c *SAPAPICaller) callPurchaseOrderSrvAPIRequirementPurchaseRequisition(api, purchaseRequisition, purchaseRequisitionItem string) ([]sap_api_output_formatter.Item, error) {
+	url := strings.Join([]string{c.baseURL, "API_PURCHASEORDER_PROCESS_SRV", api}, "/")
+
+	param := c.getQueryWithPurchaseRequisition(map[string]string{}, purchaseRequisition, purchaseRequisitionItem)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) getQueryWithHeader(params map[string]string, purchaseOrder string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchaseOrder eq '%s'", purchaseOrder)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithItem(params map[string]string, purchaseOrder, purchaseOrderItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchaseOrder eq '%s' and PurchaseOrderItem eq '%s'", purchaseOrder, purchaseOrderItem)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithItemScheduleLine(params map[string]string, purchasingDocument, purchasingDocumentItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchasingDocument eq '%s' and PurchasingDocumentItem eq '%s'", purchasingDocument, purchasingDocumentItem)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithItemPricingElement(params map[string]string, purchaseOrder, purchaseOrderItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchaseOrder eq '%s' and PurchaseOrderItem eq '%s'", purchaseOrder, purchaseOrderItem)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithItemAccount(params map[string]string, purchaseOrder, purchaseOrderItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchaseOrder eq '%s' and PurchaseOrderItem eq '%s'", purchaseOrder, purchaseOrderItem)
+	return params
+}
+
+func (c *SAPAPICaller) getQueryWithPurchaseRequisition(params map[string]string, purchaseRequisition, purchaseRequisitionItem string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("PurchaseRequisition eq '%s' and PurchaseRequisitionItem eq '%s'", purchaseRequisition, purchaseRequisitionItem)
+	return params
+}
